dbc04cf6f0bd4a0c32b688ade553715d66718a53
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-08-04 13:37:16 +0300

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,3 @@
-// const switcher = document.querySelector('#theme-switch-toggle')
-// const siteTheme = document.querySelector('body')
-
-
-// const Theme = {
-//     LIGHT: 'light-theme',
-//     DARK: 'dark-theme',
-// };
 
 export const refs = {
     switcher: document.querySelector('#theme-switch-toggle'),
